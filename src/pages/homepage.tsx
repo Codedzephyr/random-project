3303e16585d806b6fa18f01cd95658efea0b5d4d
Salihu Abdulhamid <salihu45@gmail.com>
2025-09-08 23:13:05 +0100

    slider usage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -4,6 +4,7 @@ import { Intro } from "../components/intro";
 import { Services } from "../components/home-service";
 import { Partners } from "../components/partners";
 import { VideoAbout } from "../components/video-about";
+import PartnersSlider from "../components/partners-slider";
 // import { Hero } from "../sections/home/Hero";
 // import { Intro } from "../sections/home/Intro";
 // import { Services } from "../sections/home/Services";
@@ -13,6 +14,7 @@ import { VideoAbout } from "../components/video-about";
 export const HomePage: React.FC = () => (
   <main>
     <Hero />
+    <PartnersSlider />
     <Intro />
     <Services />
     <Partners />
