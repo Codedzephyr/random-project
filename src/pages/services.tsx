d9290343aa4526bb21e7220ea3fe3c16ae63b317
Mesembe <68950482+Mesembe@users.noreply.github.com>
2025-09-08 08:07:19 +0100

    Create services.tsx
    Services page

---
+++ src/pages/services.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Services } from "../components/services";
+
+const ServicesPage: React.FC = () => (
+  <main className="bg-white">
+    <Services />
+  </main>
+);
+
+export default ServicesPage;
