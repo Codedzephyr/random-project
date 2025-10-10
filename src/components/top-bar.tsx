4dc7e738e777d65f8b64af4910c64fc72c54a9ca
Mesembe <68950482+Mesembe@users.noreply.github.com>
2025-10-10 15:37:21 +0100

    Update top-bar.tsx

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -45,7 +45,7 @@ export const TopBar: React.FC = () => {
           <img
             src="/logo.png"
             alt="LPS-HYPROPS Logo"
-            className="max-h-20"
+            className="max-h-32"
           />
         </Link>
 
